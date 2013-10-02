05efa50907299d8305b1a3a9fdc6d7025d6c721f
Ryo Nakamura <r7kamura@gmail.com>
2013-10-03 01:10:55 +0900

    Fix spec description

--- gospel_test.go
+++ gospel_test.go
@@ -5,12 +5,12 @@ import "testing"
 func TestDescribe(t *testing.T) {
 	Describe(t, "gospel.Expectation#ToEqual", func(context Context, it It) {
 		context("with 1 & 1", func() {
-			it("compares integers with ==", func(expect Expect) {
+			it("compares integers by ==", func(expect Expect) {
 				expect(1).ToEqual(1)
 			})
 		})
 		context("with `1` & `1`", func() {
-			it("compares strings with ==", func(expect Expect) {
+			it("compares strings by ==", func(expect Expect) {
 				expect("1").ToEqual("1")
 			})
 		})
@@ -18,12 +18,12 @@ func TestDescribe(t *testing.T) {
 
 	Describe(t, "gospel.Expectation#ToNotEqual", func(context Context, it It) {
 		context("with 1 & 2", func() {
-			it("compares integers with !=", func(expect Expect) {
+			it("compares integers by !=", func(expect Expect) {
 				expect(1).ToNotEqual(2)
 			})
 		})
 		context("with `1` & `2`", func() {
-			it("compares strings with !=", func(expect Expect) {
+			it("compares strings by !=", func(expect Expect) {
 				expect("1").ToNotEqual("2")
 			})
 		})
