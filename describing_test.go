17b633508fc3fb338b0436c4f8bd2cda03d0d952
Ryo Nakamura <r7kamura@gmail.com>
2013-10-03 19:22:09 +0900

    Fix typo

--- describing_test.go
+++ describing_test.go
@@ -4,13 +4,13 @@ import "testing"
 
 func TestDescribe(t *testing.T) {
 	Describe(t, "gospel.Expectation#ToEqual", func() {
-		Context("wIth 1 & 1", func() {
+		Context("with 1 & 1", func() {
 			It("compares integers by ==", func() {
 				Expect(1).ToEqual(2)
 				Expect(1).ToEqual(1)
 			})
 		})
-		Context("wIth `1` & `1`", func() {
+		Context("with `1` & `1`", func() {
 			It("compares strings by ==", func() {
 				Expect("1").ToEqual("1")
 			})
@@ -18,12 +18,12 @@ func TestDescribe(t *testing.T) {
 	})
 
 	Describe(t, "gospel.Expectation#ToNotEqual", func() {
-		Context("wIth 1 & 2", func() {
+		Context("with 1 & 2", func() {
 			It("compares integers by !=", func() {
 				Expect(1).ToNotEqual(2)
 			})
 		})
-		Context("wIth `1` & `2`", func() {
+		Context("with `1` & `2`", func() {
 			It("compares strings by !=", func() {
 				Expect("1").ToNotEqual("2")
 			})
@@ -31,21 +31,21 @@ func TestDescribe(t *testing.T) {
 	})
 
 	Describe(t, "gospel.Expectation#ToExist", func() {
-		Context("wIth 1", func() {
-			It("checks existence by non-equivalence wIth nil", func() {
+		Context("with 1", func() {
+			It("checks existence by non-equivalence with nil", func() {
 				Expect(1).ToExist()
 			})
 		})
-		Context("wIth `1`", func() {
-			It("checks existence by non-equivalence wIth nil", func() {
+		Context("with `1`", func() {
+			It("checks existence by non-equivalence with nil", func() {
 				Expect("1").ToExist()
 			})
 		})
 	})
 
 	Describe(t, "gospel.Expectation#ToNotExist", func() {
-		Context("wIth nil", func() {
-			It("checks existence by equivalence wIth nil", func() {
+		Context("with nil", func() {
+			It("checks existence by equivalence with nil", func() {
 				Expect(nil).ToNotExist()
 			})
 		})
