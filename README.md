fa3fbf3b76dc80e1cfdca58f4a48b3cea31c2564
Ryo Nakamura <r7kamura@gmail.com>
2013-10-06 22:41:48 +0900

    Modify tests for the previous interface change

--- README.md
+++ README.md
@@ -16,28 +16,28 @@ import (
 )
 
 func TestDescribe(t *testing.T) {
-	Describe(t, "gospel.Expectation#ToEqual", func() {
-		Context("with 1 & 1", func() {
-			It("compares integers by ==", func() {
+	Describe(t, "Expectation#To", func() {
+		Context("with Equal", func() {
+			It("evaluates actual == expected", func() {
 				Expect(1).To(Equal, 1)
 			})
 		})
-		Context("with `1` & `1`", func() {
-			It("compares strings by ==", func() {
-				Expect("1").To(Equal, "1")
-			})
-		})
-	})
 
-	Describe(t, "gospel.Expectation#ToNotEqual", func() {
-		Context("with 1 & 2", func() {
-			It("compares integers by !=", func() {
+		Context("with NotEqual", func() {
+			It("evaluates actual != expected", func() {
 				Expect(1).To(NotEqual, 2)
 			})
 		})
-		Context("with `1` & `2`", func() {
-			It("compares strings by !=", func() {
-				Expect(1).To(NotEqual, "2")
+
+		Context("with Exist", func() {
+			It("evaluates actual != nil", func() {
+				Expect(1).To(Exist)
+			})
+		})
+
+		Context("with NotExist", func() {
+			It("evaluates actual == nil", func() {
+				Expect(nil).To(NotExist)
 			})
 		})
 	})
@@ -54,4 +54,4 @@ $ go test
 $ go test -v
 ```
 
-![](http://dl.dropboxusercontent.com//u/5978869/image/20131003_083718.png)
+![](http://dl.dropboxusercontent.com//u/5978869/image/20131006_224123.png)
--- describing_test.go
+++ describing_test.go
@@ -3,49 +3,27 @@ package gospel
 import "testing"
 
 func TestDescribe(t *testing.T) {
-	Describe(t, "gospel.Expectation#ToEqual", func() {
-		Context("with 1 & 1", func() {
-			It("compares integers by ==", func() {
-				Expect(1).To(Equal, 1)
+	Describe(t, "Expectation#To", func() {
+		Context("with Equal", func() {
+			It("evaluates actual == expected", func() {
 				Expect(1).To(Equal, 1)
 			})
 		})
-		Context("with `1` & `1`", func() {
-			It("compares strings by ==", func() {
-				Expect("1").To(Equal, "1")
-			})
-		})
-	})
 
-	Describe(t, "gospel.Expectation#ToNotEqual", func() {
-		Context("with 1 & 2", func() {
-			It("compares integers by !=", func() {
+		Context("with NotEqual", func() {
+			It("evaluates actual != expected", func() {
 				Expect(1).To(NotEqual, 2)
 			})
 		})
-		Context("with `1` & `2`", func() {
-			It("compares strings by !=", func() {
-				Expect(1).To(NotEqual, "2")
-			})
-		})
-	})
 
-	Describe(t, "gospel.Expectation#ToExist", func() {
-		Context("with 1", func() {
-			It("checks existence by non-equivalence with nil", func() {
+		Context("with Exist", func() {
+			It("evaluates actual != nil", func() {
 				Expect(1).To(Exist)
 			})
 		})
-		Context("with `1`", func() {
-			It("checks existence by non-equivalence with nil", func() {
-				Expect("1").To(Exist)
-			})
-		})
-	})
 
-	Describe(t, "gospel.Expectation#ToNotExist", func() {
-		Context("with nil", func() {
-			It("checks existence by equivalence with nil", func() {
+		Context("with NotExist", func() {
+			It("evaluates actual == nil", func() {
 				Expect(nil).To(NotExist)
 			})
 		})
